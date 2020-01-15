9e535d220a39b4b96f94a0dc3b425efe8b3a7d69
ChidiChuks <chidiebere_chukwuma@yahoo.com>
2020-01-15 08:28:29 +0100

    updating the scss variables

---
+++ .history/src/app/home/home.page_20200113141144.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  percent:number = 50;
+  radius:number = 100;
+
+  fullTime: any = '00:01:30';
+
+}
